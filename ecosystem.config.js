60658b8327ac45d9f16f5e1567b503772bd04bf2
Kariuki47 <joseph@hostkraft.cloud>
2023-08-30 23:39:44 +0300

    added pm2 processor

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            "name": "gateway",
+            "script": "./gateway/server.js"
+        },
+        {
+            "name": "api",
+            "script": "./api/server.js"
+        }
+    ]
+}
